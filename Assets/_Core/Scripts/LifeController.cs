9e780dacd037b17eb5c226aa347243abd586576c
Logan <pix.59700@gmail.com>
2024-01-31 12:49:53 +0100

    Player Life UI

--- Assets/_Core/Scripts/LifeController.cs
+++ Assets/_Core/Scripts/LifeController.cs
@@ -5,7 +5,7 @@ using UnityEngine.Events;
 public class LifeController : MonoBehaviour
 {
     [SerializeField] private int _life = 100;
-    [SerializeField] private UnityEvent<int> _callbacksLife;
+    [SerializeField] private UnityEvent<float> _callbacksLife;
     [SerializeField] private UnityEvent _callbacksDeath;
 
     private void Update()
@@ -19,13 +19,13 @@ public class LifeController : MonoBehaviour
     public void AddLife(int addLife)
     {
         _life += addLife;
-        _callbacksLife?.Invoke(_life);
+        _callbacksLife?.Invoke(_life * .1f);
     }
 
     public void LessLife(int lessLife)
     {
         _life -= lessLife;
-        _callbacksLife?.Invoke(_life);
+        _callbacksLife?.Invoke(_life * .1f);
     }
 
     public void DestroyObject()
